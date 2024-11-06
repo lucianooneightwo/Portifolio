2c07854a00fdc77dc9f6f640b779f251e7c7adac
Luciano Siqueira <luciano.siqueira2201@gmail.com>
2024-11-05 22:45:03 -0300

    projeto portifolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-abrir').addEventListener('click', function() {
+  document.getElementById('menu-mobile').style.display = 'flex';
+});
+
+document.getElementById('btn-fechar').addEventListener('click', function() {
+  document.getElementById('menu-mobile').style.display = 'none';
+});
